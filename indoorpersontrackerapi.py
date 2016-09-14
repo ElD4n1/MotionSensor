934a97780f2160c7872b0d500a208b4fba084344
ElD4n1 <=>
2016-09-14 09:28:55 +0000

    updated indoorpersontrackerapi

--- indoorpersontrackerapi.py
+++ indoorpersontrackerapi.py
@@ -1,15 +1,26 @@
+import serverconfig
+
 class IndoorPersonTrackerAPI:
 
     def __init__(self):
         from suds.client import Client
-        url = 'http://10.0.0.8:8080/WebServices/iptwebservice?wsdl'
+        url = 'http://{}:{}/WebServices/iptwebservice?wsdl'.format(serverconfig.IP, serverconfig.PORT)
         self.client = Client(url)
 
     def register(self, identifier):
-        self.client.service.register(identifier)
-	
-    def updateDetection(self, identifier, probFalseDetection):
-        self.client.service.updateDetection(identifier, probFalseDetection)
+        return self.client.service.register(identifier)
 
-    def updateIdentification(self, identifier, personName, probFalseDetection):
-        self.client.service.updateIdentification(identifier, personName, probFalseDetection)
+    def deregister(self, identifier):
+        self.client.service.deregister(identifier)
+
+    def updateDetection(self, identifier):
+        self.client.service.updateDetection(identifier)
+
+    def updateDetectionCustomPFD(self, identifier, probFalseDetection):
+        self.client.service.updateDetectionCustomPFD(identifier, probFalseDetection)
+
+    def updateIdentification(self, identifier, personName):
+        self.client.service.updateIdentification(identifier, personName)
+
+    def updateIdentificationCustomPFD(self, identifier, personName, probFalseDetection):
+        self.client.service.updateIdentificationCustomPFD(identifier, personName, probFalseDetection)
